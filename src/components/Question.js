8a1aa0b81866db93aa8455d05f3c642e17c5341a
christian-gerard <1@christiangerard.me>
2024-03-01 13:36:22 -0700

    Trivia Update

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,58 @@
+import { useState } from 'react'
 import { useOutletContext } from 'react-router-dom';
 import QuestionCard from './QuestionCard'
 
 function Question() {
     const questions = useOutletContext()
+    const [questionFilter, setQuestionFilter ] = useState('')
+    const [questionNumber, setQuestionNumber] = useState(0)
 
-    const renderQuestion = questions.map((question) => <QuestionCard key={question.id} {...question} />)
+    const handleFilter = (newFilter) => {
+        setQuestionFilter((questionFilter) =>  newFilter )
+        setQuestionNumber(0)
+    }
+    const handleNext = () => {
+
+        setQuestionNumber(questionNumber =>  renderQuestion().length - 1 <= questionNumber ? 0 : questionNumber += 1)
+        console.log(questionNumber)
+    }
+
+
+    const renderQuestion = () => {
+
+        if(questionFilter === '') {
+            
+            return questions.map((question) => <QuestionCard 
+            key={question.id} 
+            {...question} 
+            handleNext={handleNext} 
+            questionNumber={questionNumber} />)
+        } else {
+
+          return   questions.filter((question) => question.type === questionFilter).map((question) => <QuestionCard 
+          key={question.id} 
+          {...question} 
+          handleNext={handleNext} 
+          questionNumber={questionNumber} />)
+        }
+
+
+    }
+    
+
 
     return(
         <div>
             <h1>Questions</h1>
+            <select type='dropdown' onChange={(e) => handleFilter(e.target.value)} >
+                <option value=''>All</option>
+                <option value='geography'>Geography</option>
+                <option value='movies'>Movies</option>
+                <option value='history'>History</option>
+            </select>
+
             <div>
-                {renderQuestion}
+                {renderQuestion()[questionNumber]}
             </div>
         </div>
     )
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 
-function QuestionCard({id,type,question,answer1,answer2,answer3,answer4,correctAnswer}) {
-
+function QuestionCard({id,type,question,answer1,answer2,answer3,answer4,correctAnswer,handleNext,questionNumber}) {
+    const [selectedAnswer, setSelectedAnswer] = useState('')
     const [isCorrect, setIsCorrect] = useState(false);
+
 
     const checkAnswer = (e) => {
     
         if(e.target.name === correctAnswer) {
+            setSelectedAnswer(selectedAnswer => e.target.value)
+            setIsCorrect(true)
             
-            console.log('CORRECT')
-            e.target.checked = false;
         } else {
+            setSelectedAnswer(selectedAnswer => e.target.value)
             console.log('WRONG')
         }
         
@@ -19,15 +21,34 @@ function QuestionCard({id,type,question,answer1,answer2,answer3,answer4,correctA
 
     return(
         <div className='question-card'>
+            <h2>Q.{questionNumber + 1}</h2>
             <h1>{question}</h1>
-
             <div className='answers' onChange={checkAnswer}>
-                <span><input name='answer1' type="checkbox"/>{answer1}</span>
-                <span><input name='answer2' type="checkbox"/>{answer2}</span>
-                <span><input name='answer3' type="checkbox"/>{answer3}</span>
-                <span><input name='answer4' type="checkbox"/>{answer4}</span>
+                {isCorrect ? <h1>"{selectedAnswer}"" is Correct</h1> : selectedAnswer ?
+                <>
+                <h1>{selectedAnswer} is Incorrect😔</h1>
+                <span><input name='answer1' type="checkbox" value={answer1}/>{answer1}</span>
+                <span><input name='answer2' type="checkbox" value={answer2}/>{answer2}</span>
+                <span><input name='answer3' type="checkbox" value={answer3}/>{answer3}</span>
+                <span><input name='answer4' type="checkbox" value={answer4}/>{answer4}</span>
+                </>
+
+                :
+
+                <>
+                
+                <span><input name='answer1' type="checkbox" value={answer1}/>{answer1}</span>
+                <span><input name='answer2' type="checkbox" value={answer2}/>{answer2}</span>
+                <span><input name='answer3' type="checkbox" value={answer3}/>{answer3}</span>
+                <span><input name='answer4' type="checkbox" value={answer4}/>{answer4}</span>
+                </>
+                
+                
+                
+                }
+                
             </div>
-            
+            {isCorrect ? <button onClick={handleNext}>NEXT</button> : null}
         </div>
     )
 }
